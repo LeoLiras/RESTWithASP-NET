3ca4cde6ea4eb2b1abcc7e11bdff23b82488ab63
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2025-02-12 13:39:55 -0300

    Implement database Migration

--- RESTWithASP-NET/RESTWithASP-NET/Program.cs
+++ RESTWithASP-NET/RESTWithASP-NET/Program.cs
@@ -4,6 +4,10 @@ using RESTWithASP_NET.Business.Implementations;
 using RESTWithASP_NET.Business;
 using RESTWithASP_NET.Repository;
 using RESTWithASP_NET.Repository.Implementations;
+using Microsoft.AspNetCore.Hosting;
+using MySqlConnector;
+using EvolveDb;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +19,11 @@ var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 
 builder.Services.AddDbContext<MySQLContext>(options => 
                                                 options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
 
 builder.Services.AddApiVersioning();
 
@@ -32,3 +41,23 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void MigrateDatabase(string? connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true
+        };
+
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+
+        throw;
+    }
+}
--- RESTWithASP-NET/RESTWithASP-NET/Repository/Implementations/PersonRepositoryImplementation.cs
+++ RESTWithASP-NET/RESTWithASP-NET/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -34,7 +34,7 @@ namespace RESTWithASP_NET.Repository.Implementations
 
         public Person Update(Person person)
         {
-            if (!Exists(person.Id)) { return new Person(); }
+            if (!Exists(person.Id)) { return null; }
 
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
 
